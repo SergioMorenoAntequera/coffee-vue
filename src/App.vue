38afabd448adb8c71849d72f56525b749012f682
Sergio <seranmoreno500@gmail.com>
2020-04-21 17:24:17 +0200

    Vue actualizado a bootstrap
    asd

--- src/App.vue
+++ src/App.vue
@@ -18,11 +18,15 @@ export default {
 
 <style>
 #app {
+  width: 100%;
+  min-height: 100vh;
+
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
 
   background-color: #F7DFBD;
 }
--- src/components/TitleAndImages.vue
+++ src/components/TitleAndImages.vue
@@ -1,64 +1,62 @@
 <template>
   <div class="titleAndImages">
     <div class="titleAndImagesContent">
-      <div class="title">
-        <h1> {{ title }}</h1>
+      
+      <!-- title  -->
+      <div class="row justify-content-center">
+        <div class="title col-6">
+          <h1> {{ title }}</h1>
+        </div>
       </div>
-      <div class="images"></div>
+
+      <!-- Images -->
+      <transition name="animate" 
+      enter-active-class="animated fadeInDown"
+      leave-active-class="animated fadeOutDown">
+          
+        <div class="images row justify-content-center">
+          <div class="col-7 col-md-4 px-0 px-md-4 bg-primary">
+            PRA
+          </div>
+          <div class="col-7 col-md-4 px-0 px-md-4 bg-secondary">
+            PRA
+          </div>
+          <div class="col-7 col-md-4 px-0 px-md-4 bg-primary">
+            PRA
+          </div>
+        </div>
+
+      </transition>
     </div>
   </div>
 </template>
 
 <script>
 // import image from "./assets/logo.png";
-
 export default {
   name: "TitleAndImagesContent",
   props: ['title'],
-  data: function() {
+  data: () => {
     return {
-      //image: image
+      // PRARIABLES
     };
   }
 };
 </script>
 
 <style lang="css" scoped>
-.titleAndImages {
-  margin: 0px;
-  padding: 0px;
-  box-sizing: border-box;
-  position: relative;
-  display: flex;
-  justify-content: center;
-  min-height: 500px;
-  min-width: 100%;
-}
-.titleAndImages .titleAndImagesContent {
-  position: relative;
-  display: flex;
-  justify-content: center;
-  width: 90%;
-}
 .titleAndImages .titleAndImagesContent .title {
   position: relative;
-  width: 45%;
-  height: 50px;
-
-  text-align: center;
-  background-color: #4B423C;
-  padding: 65px 30px 130px 30px;
+  background-color: #4B423C; 
 }
 .titleAndImages .titleAndImagesContent .title h1 {
-  display: inline-block;
+  padding-top: 70px;
+  padding-bottom: 100px;
   font-weight: bolder;
   color: #BB9C79;
 }
 .titleAndImages .titleAndImagesContent .images {
-  position: absolute;
-  top: 120px;
-  width: 100%;
-  height: 50px;
-  background-color: red;
+  position: relative;
+  top: -100px;
 }
 </style> 
